5e89fa9b24fd15aa5b0bfdfad372b2ad3a119a90
Proac-Tee <babatundetaiwo038@gmail.com>
2026-07-24 23:15:14 +0100

    chore(app): scallfolded required app structure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from fast-api!")
-
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app="app:app", app_dir="src", host="0.0.0.0", port=8000, reload=True)
